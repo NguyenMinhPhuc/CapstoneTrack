d14ac634dc168c80a16949ef499d70c47f53a7d3
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-03 07:52:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7033056987-c6fd4",
+  "appId": "1:361814519743:web:9032a91a3830b57af9bb0a",
+  "apiKey": "AIzaSyAm9SBuGqItIShubrb-iyiriCpbLcYb9LI",
+  "authDomain": "studio-7033056987-c6fd4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "361814519743"
+};
