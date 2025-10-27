a99e39d0f59cacab46a399496e15756068a3c58e
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-27 08:00:53 +0000

    sau khi chỉnh các chức năng hiện tài không còn trên menu nữa

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -29,7 +29,6 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
-export * from './non-blocking-login';
 
 // Re-exporting setDoc and other functions with a different name to avoid conflicts if needed
 export { setDoc as setDocument, doc as getDocRef, serverTimestamp as getTimestamp } from 'firebase/firestore';
